a90da1096cd16d51551b8579824dde92cd20bf9c
WooDaeHyun <po668312@naver.com>
2022-11-02 00:12:34 +0900

    feat : Add HTML

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,18 @@
 // params.$target - 해당 컴포넌트가 추가가 될 DOM 요소 노드
 // params.initialState - 해당 컴포넌트의 초기 상태
-function TodoList(params) {
+function TodoList({ $target, initialState }) {
   const $todoList = document.createElement('div');
-  const $target = params.$target;
-  $target.appenChild($todoList);
+  $target.appendChild($todoList);
 
-  this.state = params.initialState;
+  this.state = initialState;
 
   this.render = () => {
     $todoList.innerHTML = `
       <ul>
-        ${this.state.map(todo => `<li>${todo.text}</li>`).join('')}
+        ${this.state.map(({ text }) => `<li>${text}</li>`).join('')}
       </ul>
     `
-
-    
   }
+
+  this.render();
 }
